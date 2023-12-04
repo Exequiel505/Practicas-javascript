010c189caab56cd05fbc67588559e91035f6b292
Exequiel <exequielguardia@gmail.com>
2023-12-04 14:57:22 -0300

    finish

--- src/server.js
+++ src/server.js
@@ -9,8 +9,24 @@ const readProducts = carrito.readProducts();
 
 app.get("/products",async (req, res) =>{
 
-    res.send(await readProducts)
+    let limit = parseInt(req.query.limit);
 
+    if (!limit) return res.send(await readProducts);
+
+    let allProducts = await readProducts;
+
+    let proudctLimit =  allProducts.slice(0,limit);
+
+    res.send(proudctLimit);
+
+});
+
+app.get("/products/:id",async (req, res) =>{
+
+    let id = parseInt(req.params.id);
+    let allProducts = await readProducts;
+    let productById = allProducts.find (product => product.id ===id)
+    res.send(productById);
 });
 
 const PORT = 8080
