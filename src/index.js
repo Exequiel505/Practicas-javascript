36f25536e6ffea866a4c1c0c4895c1d023f50cd0
Exequiel <exequielguardia@gmail.com>
2023-12-12 20:19:57 -0300

    prueba-2

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { ProductManager } from "./productManager.js"
+import { productsRouter } from "./routes/products.router.js";
+const PORT = 8080
+const app = express();
+app.use(express.json())
+app.use("/api/products", productsRouter)
+app.listen(PORT, (req,res)=>{
+    console.log(`servidor escuchando en el puerto ${PORT}`)
+})
+export const productManager = new ProductManager
